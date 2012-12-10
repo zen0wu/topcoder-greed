39caa8ebe872c7321df87874840b5ec07384909b
Shiva Wu <shivaw@gmail.com>
2012-12-10 23:09:13 +0800

    fix run_arena script

--- dbg/run_arena.sh
+++ dbg/run_arena.sh
@@ -8,4 +8,4 @@ echo "development mode: $DEVELOP_MODE"
 echo "current path: `pwd`"
 
 #java -DdevelopmentMode=$DEVELOP_MODE -DprojectDir=`pwd` -cp lib/ContestApplet.jar com.topcoder.client.contestApplet.runner.generic www.topcoder.com 5001 http://tunnel1.topcoder.com/tunne?dummy TopCoder
-javaws -J-DdevelopmentMode=$DEVELOP_MODE -J-DprojectDir=`pwd` lib/ContestAppletProd.jnlp
+javaws -J-DdevelopmentMode=$DEVELOP_MODE -J-DprojectDir=`pwd` ContestAppletProd.jnlp
