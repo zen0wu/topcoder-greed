eb2e75c7f49046dd5d312e2bd0e90dbe06c5ff7f
Rajat Khandelwal <rajatgupta59@gmail.com>
2013-06-14 19:10:33 +0530

    Added the debugger

--- src/main/resources/Resource/Template.cpp
+++ src/main/resources/Resource/Template.cpp
@@ -5,9 +5,16 @@
 #include <iostream>
 #include <algorithm>
 #include <set>
+#include <map>
 #include <vector>
 #include <sstream>
 #include <typeinfo>
+
+#ifdef DEBUG
+    #define debug(args...) {dbg,args;std::cout<<std::endl;}
+#else
+    #define debug(args...) // Just strip off all debug tokens
+#endif
 
 using namespace std;
 
