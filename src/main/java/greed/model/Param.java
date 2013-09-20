5f536d514d3f4b42bcc7bcc3b12c424db1c3eeb9
vexorian <vx@vx.(none)>
2013-09-20 19:43:07 -0400

    Add .Num variable to the Parameter object for templates.

--- src/main/java/greed/model/Param.java
+++ src/main/java/greed/model/Param.java
@@ -6,10 +6,12 @@ package greed.model;
 public class Param {
     private String name;
     private Type type;
+    private int num;
 
-    public Param(String name, Type type) {
+    public Param(String name, Type type, int num) {
         this.name = name;
         this.type = type;
+        this.num = num;
     }
 
     public String getName() {
@@ -18,5 +20,9 @@ public class Param {
 
     public Type getType() {
         return type;
+    }
+    
+    public int getNum() {
+        return num;
     }
 }
