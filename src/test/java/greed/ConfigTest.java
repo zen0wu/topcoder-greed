c41fc98af455670bdb852787a8e16ad9f6d147b0
Shiva Wu <shivaw@gmail.com>
2013-10-16 12:07:18 +0800

    update testing configuration

--- src/test/java/greed/ConfigTest.java
+++ src/test/java/greed/ConfigTest.java
@@ -11,6 +11,10 @@ public class ConfigTest {
         if (conf.hasPath("greed"))
             System.out.println(conf.getConfig("greed").toString());
         System.out.println(conf.getConfig("greed.lang.cpp").resolve().toString());
+        Config config = conf.getConfig("greed.templateDefs.testcases.afterGen").resolve();
+        for (String s: config.getStringList("arguments")) {
+            System.out.println(s);
+        }
 
 
         conf = ConfigFactory.parseFile(new File(System.getProperty("user.dir") + "/src/main/resources/default.conf")).resolve();
