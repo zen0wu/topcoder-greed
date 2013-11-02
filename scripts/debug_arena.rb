9fbde4886fd63b5d4654908786f1a01107272ea1
Shiva Wu <shivaw@gmail.com>
2013-11-02 09:17:39 +0800

    new debug script

---
+++ scripts/debug_arena.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+HOME_DIR = File.expand_path(File.dirname(__FILE__) + "/..")
+
+require 'optparse'
+
+java_opts = ["-J-DprojectDir=#{HOME_DIR}"]
+
+ARGV.options do |opts|
+	opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS]"
+	
+	opts.separator ""
+	opts.separator "Common Options:"
+	
+	opts.on( "-h", "--help",
+	         "Show this message." ) do
+		puts opts
+		exit
+	end
+
+	opts.on( "-d", "--debug",
+			  "Enable debug environment when running the arena" ) do
+		java_opts << '-J-DdevelopmentMode=true'
+	end
+
+	opts.on( "-i", "--idea-debug",
+			 "Enable idea remote debugging while running the arena" ) do
+		java_opts << '-J-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005'
+	end
+
+	begin
+		opts.parse!
+	rescue
+		puts opts
+		exit
+	end
+end
+
+puts "Executing javaws with argumnets: "
+puts java_opts.map{ |x| "  " + x }.join "\n"
+%x[javaws #{java_opts.join ' '} #{HOME_DIR}/scripts/ContestAppletProd.jnlp]
