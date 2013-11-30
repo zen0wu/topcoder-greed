eb395b08231794c7eaf912dda7a41c65c46b33ce
Jongwook Choi <wookayin@gmail.com>
2013-11-30 14:09:18 +0900

    Add predefined Type instances, and polish tests
    This commit introduces some predefined constant instances of class Type
    (e.g. INT_TYPE, INT_ARRAY_TYPE, ...), so that redundant codes such as
    `new Type(INT, 0)` can be suppressed. We now have much simpler test codes.

--- src/main/java/greed/model/Type.java
+++ src/main/java/greed/model/Type.java
@@ -4,13 +4,39 @@ package greed.model;
  * Greed is good! Cheers!
  */
 public class Type {
-    private Primitive primitive;
-    private int dim;
+    private final Primitive primitive;
+    private final int dim;
 
-    public Type(Primitive primitive, int dim) {
+    Type(Primitive primitive, int dim) {
         this.primitive = primitive;
         this.dim = dim;
     }
+
+
+    public static Type primitiveType(Primitive primitive) {
+        switch(primitive) {
+            case BOOL: return BOOL_TYPE;
+            case INT: return INT_TYPE;
+            case STRING: return STRING_TYPE;
+            case LONG: return LONG_TYPE;
+            case DOUBLE: return DOUBLE_TYPE;
+        }
+        throw new IllegalArgumentException("Unknown primitive : " + primitive);
+    }
+
+    /* some pre-defined types */
+    public static final Type INT_TYPE = new Type(Primitive.INT, 0);
+    public static final Type STRING_TYPE = new Type(Primitive.STRING, 0);
+    public static final Type LONG_TYPE = new Type(Primitive.LONG, 0);
+    public static final Type DOUBLE_TYPE = new Type(Primitive.DOUBLE, 0);
+    public static final Type BOOL_TYPE = new Type(Primitive.BOOL, 0);
+
+    public static final Type INT_ARRAY_TYPE = new Type(Primitive.INT, 1);
+    public static final Type STRING_ARRAY_TYPE = new Type(Primitive.STRING, 1);
+    public static final Type LONG_ARRAY_TYPE = new Type(Primitive.LONG, 1);
+    public static final Type DOUBLE_ARRAY_TYPE = new Type(Primitive.DOUBLE, 1);
+    public static final Type BOOL_ARRAY_TYPE = new Type(Primitive.BOOL, 1);
+
 
     public Primitive getPrimitive() {
         return primitive;
