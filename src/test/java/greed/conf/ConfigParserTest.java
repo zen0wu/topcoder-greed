0c9f033d1d3030431be4493a87e7ae9d015f5b86
Shiva Wu <shivaw@gmail.com>
2014-01-04 15:29:43 +0800

    close #120: load configuration with specifying classloader

--- src/test/java/greed/conf/ConfigParserTest.java
+++ src/test/java/greed/conf/ConfigParserTest.java
@@ -8,15 +8,17 @@ import org.junit.Test;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.util.Arrays;
+
 public class ConfigParserTest {
     @Test
     public void testSerialize() throws ConfigException {
         ConfigParser configParser = new ConfigParser();
-        Config config = ConfigFactory.parseURL(ConfigParserTest.class.getResource("/default.conf")).resolve();
+        Config config = ConfigFactory.load("default").resolve();
         System.out.println(config.toString());
         GreedConfig greedConfig = configParser.parseAndCheck("greed", config.getConfig("greed"), GreedConfig.class);
         System.out.println(StringUtil.join(greedConfig.getLanguage().get("java").getTemplateDef().get("source").getTransformers(), ", "));
-        System.out.println(greedConfig.getLanguage().get("java").getTemplateDef().get("source").getDependencies());
+        System.out.println(Arrays.toString(greedConfig.getLanguage().get("java").getTemplateDef().get("source").getDependencies()));
         System.out.println(greedConfig.getLanguage().get("java").getTemplateDef().get("testcase").getOverwrite());
         System.out.println("done");
     }
